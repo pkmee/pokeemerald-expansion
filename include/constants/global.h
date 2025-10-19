fda689e371f5a7aff3f44cb6263f1e5ab33fd59a
fisham <joshdennis93@gmail.com>
2025-10-19 23:39:18 +1100

    save block modification

--- include/constants/global.h
+++ include/constants/global.h
@@ -77,10 +77,10 @@
 #define ROAMER_COUNT 1 // Number of maximum concurrent active roamers
 
 // Bag constants
-#define BAG_ITEMS_COUNT 30
+#define BAG_ITEMS_COUNT 80
 #define BAG_KEYITEMS_COUNT 30
 #define BAG_POKEBALLS_COUNT 16
-#define BAG_TMHM_COUNT 64
+#define BAG_TMHM_COUNT 108
 #define BAG_BERRIES_COUNT 46
 
 // Number of facilities for Ranking Hall.
