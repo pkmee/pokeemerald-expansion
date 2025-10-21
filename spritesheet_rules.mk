ae13a9a172a6f2c71298045beee5a6d37bde9411
fisham <joshdennis93@gmail.com>
2025-10-21 21:41:39 +1100

    insert tm item ball

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -4934,3 +4934,7 @@ $(OBJEVENTGFXDIR)/misc/ball_%.4bpp: $(OBJEVENTGFXDIR)/misc/ball_%.png ; $(GFX) $
 
 graphics/door_anims/battle_tower_multi_corridor.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
+
+# jd:
+$(OBJEVENTGFXDIR)/misc/ball_tm.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 2 -mheight 4
